d7e1271fa6c07fd8353d18bc6d67c8d3bd8f349a
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-24 22:58:44 +0530

    All Activity Lifecycles

--- app/src/main/java/com/sagardhadke/activitylifecycle/MainActivity.kt
+++ app/src/main/java/com/sagardhadke/activitylifecycle/MainActivity.kt
@@ -2,10 +2,36 @@ package com.sagardhadke.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        Toast.makeText(this, "onStart", Toast.LENGTH_SHORT).show()
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Toast.makeText(this, "onResume", Toast.LENGTH_SHORT).show()
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Toast.makeText(this, "onPause", Toast.LENGTH_SHORT).show()
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Toast.makeText(this, "onStop", Toast.LENGTH_SHORT).show()
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Toast.makeText(this, "onDestroy", Toast.LENGTH_SHORT).show()
+        super.onDestroy()
     }
 }
