ccdad0bac52a697135b0ae4040c45e1180081390
Sagar Dhadke <peaceunique2003@gmail.com>
2024-01-25 22:54:53 +0530

    Main Activity 2 Life Cycle

--- app/src/main/java/com/sagardhadke/activitylifecycle/MainActivity2.kt
+++ app/src/main/java/com/sagardhadke/activitylifecycle/MainActivity2.kt
@@ -2,10 +2,36 @@ package com.sagardhadke.activitylifecycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Toast.makeText(this, "onStart MainActivity2", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        Toast.makeText(this, "onResume MainActivity2", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Toast.makeText(this, "onPause MainActivity2", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Toast.makeText(this, "onStop MainActivity2", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Toast.makeText(this, "onDestroy MainActivity2", Toast.LENGTH_SHORT).show()
     }
 }
